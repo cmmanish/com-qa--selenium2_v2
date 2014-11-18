b95cb512d8a344874e0a38c336e0dfa4d96c9616
cmmanish <cmmanish@gmail.com>
2014-11-18 10:19:32 -0800

    cleared the unused imports

--- src/test/java/com/marin/qa/selenium/campaigns/BAT.java
+++ src/test/java/com/marin/qa/selenium/campaigns/BAT.java
@@ -1,14 +1,11 @@
 package com.marin.qa.selenium.campaigns;
 
-import java.util.Arrays;
 import java.util.Random;
 
 import org.apache.log4j.Logger;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import com.marin.qa.selenium.common.QaRandom;
 
 public class BAT {
 
