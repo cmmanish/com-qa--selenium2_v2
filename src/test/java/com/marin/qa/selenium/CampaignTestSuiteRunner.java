77a2a6ac7ad5bec11763f0ae246c294fa63f8513
cmmanish <cmmanish@gmail.com>
2014-11-19 16:52:56 -0800

    fixed CampaignTestSuiteRunner

--- src/test/java/com/marin/qa/selenium/CampaignTestSuiteRunner.java
+++ src/test/java/com/marin/qa/selenium/CampaignTestSuiteRunner.java
@@ -1,17 +1,17 @@
 package com.marin.qa.selenium;
 
-import com.marin.qa.selenium.campaigns.BuildAcceptanceTest;
-import com.marin.qa.selenium.campaigns.BulkAddCampaignsTest;
-import com.marin.qa.selenium.campaigns.SingleCreateCampaignsTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
+import com.marin.qa.selenium.campaigns.BuildAcceptanceTest;
+import com.marin.qa.selenium.campaigns.SingleCreateCampaignsTest;
 
 
 @RunWith(Suite.class)
 @SuiteClasses({
         BuildAcceptanceTest.class,
-        SingleCreateCampaignsTest.class,
+        SingleCreateCampaignsTest.class	
        /*  BulkAddCampaignsTest.class*/
 })
 public class CampaignTestSuiteRunner {
